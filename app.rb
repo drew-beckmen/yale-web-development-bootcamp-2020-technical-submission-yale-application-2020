583669ca74c42ca4da6f98982d32e22a61fa698d
Drew Beckmen <drew.beckmen@yale.edu>
2020-04-25 15:55:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,10 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
-
+name = "Drew"
+age = 19
+favorite_food = "pizza"
+height_in_inches = 
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
