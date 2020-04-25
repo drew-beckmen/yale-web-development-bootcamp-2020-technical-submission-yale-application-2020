d89916d13f7054636cbc6e02d58e08d9cd0776de
Drew Beckmen <drew.beckmen@yale.edu>
2020-04-25 15:57:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ name = "Drew"
 age = 19
 favorite_food = "pizza"
 height_in_inches = 74
-height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average" : "You are not taller than average"
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
